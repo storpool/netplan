fcb9e8f2ff2b640b69283ea20e7efe57e3056ca3
Jeremy JACQUE <jeremy.jacque@algolia.com>
2022-02-09 10:24:53 +0100

    Update lib version

--- netplan/config.py
+++ netplan/config.py
@@ -31,7 +31,7 @@ class NetPlan(object):
     interface names to netplan.interface.* classes.
     """
 
-    VERSION = "0.3.3"
+    VERSION = "0.4.1"
 
     def __init__(self, data):
         # type: (NetPlan, Dict[str, Any]) -> None
